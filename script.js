d96573fc9391b047acdd54e717395e75751259b2
Emmanueeel <imaanii@mail.ru>
2025-10-15 22:57:14 +0300

    script.js added

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = 'пароль';
+let userInput = prompt('Введите пароль');
+if (userInput === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
